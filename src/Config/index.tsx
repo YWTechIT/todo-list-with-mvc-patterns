8d392a567f01cbfffad238b3eef7bb351610971a
YWTechIT <abcd8637@naver.com>
2021-07-29 12:44:04 +0900

    ADD TODO-CONST

---
+++ src/Config/index.tsx
@@ -0,0 +1,19 @@
+import { Todo } from "../types";
+
+export const TODO_CONST: Todo[] = [
+    {
+      id: "1",
+      text: "타입스크립트 재밌다.",
+      done: false,
+    },
+    {
+      id: "2",
+      text: "수영 재밌다.",
+      done: false,
+    },
+    {
+      id: "3",
+      text: "게임 재밌다.",
+      done: false,
+    },
+  ];
